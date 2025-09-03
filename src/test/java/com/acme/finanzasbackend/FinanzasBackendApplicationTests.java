eb2207171d787c2c97a31aa043315461cb4a6ef7
natalia <natalia.roman.cruz@gmail.com>
2025-09-03 16:28:07 -0500

    Initial commit

---
+++ src/test/java/com/acme/finanzasbackend/FinanzasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.finanzasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
