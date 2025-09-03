8873a8630eb3cc0b4de575b34470a36d0bc7cf6e
natalia <natalia.roman.cruz@gmail.com>
2025-09-03 16:44:55 -0500

    Add dependencies and shared BC

--- src/main/java/com/acme/finanzasbackend/FinanzasBackendApplication.java
+++ src/main/java/com/acme/finanzasbackend/FinanzasBackendApplication.java
@@ -2,7 +2,9 @@ package com.acme.finanzasbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class FinanzasBackendApplication {
 
