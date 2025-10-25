fc818d3a451cb615f7a1aab049983a32f4b40d17
natalia <natalia.roman.cruz@gmail.com>
2025-10-24 23:59:33 -0500

    Add get all clients bases

--- src/main/java/com/acme/finanzasbackend/clientManagement/domain/services/ClientQueryService.java
+++ src/main/java/com/acme/finanzasbackend/clientManagement/domain/services/ClientQueryService.java
@@ -1,10 +1,13 @@
 package com.acme.finanzasbackend.clientManagement.domain.services;
 
 import com.acme.finanzasbackend.clientManagement.domain.model.aggregates.Client;
+import com.acme.finanzasbackend.clientManagement.domain.model.queries.GetAllClientsQuery;
 import com.acme.finanzasbackend.clientManagement.domain.model.queries.GetClientByIdQuery;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClientQueryService {
     Optional<Client> handle(GetClientByIdQuery query);
+    List<Client> handle(GetAllClientsQuery query);
 }
--- src/main/java/com/acme/finanzasbackend/clientManagement/infrastructure/persistence/jpa/repositories/ClientRepository.java
+++ src/main/java/com/acme/finanzasbackend/clientManagement/infrastructure/persistence/jpa/repositories/ClientRepository.java
@@ -1,9 +1,11 @@
 package com.acme.finanzasbackend.clientManagement.infrastructure.persistence.jpa.repositories;
 
 import com.acme.finanzasbackend.clientManagement.domain.model.aggregates.Client;
+import com.acme.finanzasbackend.shared.domain.model.valueobjects.RealStateCompanyId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +13,5 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
     Optional<Client> findById(Long id);
     Boolean existsByFirstnameAndLastname(String firstName, String lastName);
     Boolean existsByDni(String dni);
+    List<Client> findAllByRealStateCompanyId(RealStateCompanyId realStateCompanyId);
 }
