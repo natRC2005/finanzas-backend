74d9d455e4a167a291a17b3325ca0f42b095be1d
natalia <natalia.roman.cruz@gmail.com>
2025-11-22 01:25:20 -0500

    Continue adding generatePayment calculus

--- src/main/java/com/acme/finanzasbackend/creditSimulation/interfaces/rest/transform/PaymentResourceFromEntityAssembler.java
+++ src/main/java/com/acme/finanzasbackend/creditSimulation/interfaces/rest/transform/PaymentResourceFromEntityAssembler.java
@@ -7,6 +7,7 @@ public class PaymentResourceFromEntityAssembler {
     public static PaymentResource toResourceFromEntity(Payment entity) {
         return new PaymentResource(
                 entity.getId(),
+                entity.getPaymentDate(),
                 entity.getOrderNumber(),
                 entity.getTem(),
                 entity.getGracePeriodType(),
